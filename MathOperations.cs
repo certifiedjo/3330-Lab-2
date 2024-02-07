ae8109d427b9c6a2c75279298daea3f203a5c12d
certifiedjo <josiahspringer9@gmail.com>
2024-02-06 23:00:25 -0500

    Update Library and App

---
+++ MathOperations.cs
@@ -0,0 +1,20 @@
+﻿//CPS*3330*01
+//Josiah Springer, Pedro Romero
+namespace Lab2MathLibrary
+{
+    public class MathOperations
+    {
+        // Method to multiply two numbers
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        // Overloaded method to multiply three numbers
+        public static double Multiply(double num1, double num2, double num3)
+        {
+            return num1 * num2 * num3;
+        }
+    }
+
+}
